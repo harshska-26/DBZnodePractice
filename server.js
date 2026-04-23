d43826c7840f04775d8bc56ee6c5a93ad58f8a40
harshka-26 <harshithbabu@criskasecurity.com>
2026-04-24 04:54:05 +0530

    get update

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const { characters } = require("./Characters");
 const { CharValidFunc } = require("./Validations/CharValidation");
 const { AddCharCtrl } = require("./controllers/AddChar.controller");
-const { DelCharFunc, DelCharValFunc } = require("./Validations/DelCharValidation");
+const {
+  DelCharFunc,
+  DelCharValFunc,
+} = require("./Validations/DelCharValidation");
 const { DelCharCtrlFunc } = require("./controllers/DelChar.Controller");
 const { AddDelFunc } = require("./controllers/AddDelChar.controller");
 const { DefaultFilePath } = require("./constants/constants");
@@ -10,18 +14,17 @@ const { DefaultFilePath } = require("./constants/constants");
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
-/*app.get("/GetChar", (req, res) => {
-    res.status(200).json(characters)
-})*/
-
-
-app.post("/AddCharacters" , CharValidFunc, AddCharCtrl)
-app.post("/DeleteCharacters", DelCharValFunc, DelCharCtrlFunc)
-
+app.get("/", (req, res) => {res.status(200).json(characters);});
+app.post("/AddCharacters", CharValidFunc, AddCharCtrl);
+app.post("/DeleteCharacters", DelCharValFunc, DelCharCtrlFunc);
 
 // app.post("/AddDelCharacters", AddDelFunc)
-
 
 const PORT = 8000;
 app.listen(PORT, () => {
